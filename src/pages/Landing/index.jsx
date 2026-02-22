63ecd13c5d9f3b957129991dd429e931983a6170
lunayokai <lunalisarazo@gmail.com>
2026-02-22 13:11:08 -0300

    Acomoda landing page

--- src/pages/Landing/index.jsx
+++ src/pages/Landing/index.jsx
@@ -10,26 +10,32 @@ const Landing = () => {
     <div>
       <Background />
       <div className={styles.viewport} onClick={() => navigate("/game")}>
-        <h1 className={styles.title}>
-          {landingText.title}
-        </h1>
-        <h2 className={styles.introduction}>
-          <text className={styles.paragraph}>
-            {landingText.introduction.firstParagraph}
-          </text>
-          <text className={styles.paragraph}>
-            {landingText.introduction.secondParagraph}
-          </text>
-          <text className={styles.paragraph}>
-            {landingText.introduction.thirdParagraph}
-          </text>
-          <text className={styles.godspeed}>
-            {landingText.introduction.godspeed}
-          </text>
-        </h2>
-        <h3 className={styles.credits} >{landingText.credits.map((credit, index) => (
-          <div key={index}>{credit}</div>
-        ))}</h3>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            {landingText.title}
+          </h1>
+          <h2 className={styles.introduction}>
+            <text className={styles.paragraph}>
+              {landingText.introduction.firstParagraph}
+            </text>
+            <text className={styles.paragraph}>
+              {landingText.introduction.secondParagraph}
+            </text>
+            <text className={styles.paragraph}>
+              {landingText.introduction.thirdParagraph}
+            </text>
+            <text className={styles.godspeed}>
+              {landingText.introduction.godspeed}
+            </text>
+          </h2>
+        </div>
+        <footer className={styles.footer}>
+          <h3 className={styles.credits} >{landingText.credits.map((credit, index) => (
+            <div key={index}>{credit}</div>
+          ))}</h3>
+        </footer>
+
+
       </div>
     </div>
   );
